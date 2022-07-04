12ed4be763b9857f881d54219e3cafdf927125ca
izabelaoska <91794571+izabelaoska@users.noreply.github.com>
2022-07-04 16:39:39 +0100

    refactor: jsx Form moved to Form component

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Input } from "../common/Input";
 
 export default function Form({ children }) {
-  return <div>{children}</div>;
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [securityCode, setSecurityCode] = useState("");
+  const [name, setName] = useState("");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="mainForm" onSubmit={handleOnSubmit}>
+      <h1>ADD NEW CARD</h1>
+      <div className="border" />
+      <div className="item">
+        <Input label="CARD NUMBER" />
+      </div>
+      <div className="centered">
+        <div className="item">
+          <Input label="EXPIRY DATE" placeholder="MM/YY" />
+        </div>
+        <div className="item">
+          <Input label="CCV/CVC" />
+        </div>
+      </div>
+      <div className="item">
+        <Input label="CARDHOLDER'S NAME" />
+      </div>
+      <div>
+        <label className="checkbox label">
+          <input type="checkbox" style={{ width: "auto" }} />
+          SAVE CARD
+        </label>
+      </div>
+      <div>
+        <button>ADD CARD</button>
+      </div>
+    </form>
+  );
 }
--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,40 +1,14 @@
 import React from "react";
 
 import Card from "../Card/Card";
-import { Input } from "../common/Input";
+import Form from "../Form/Form";
 import "./MainPage.css";
 
 export default function MainPage() {
   return (
     <>
       <Card>
-        <form className="mainForm">
-          <h1>ADD NEW CARD</h1>
-          <div className="border" />
-          <div className="item">
-            <Input label="CARD NUMBER" />
-          </div>
-          <div className="centered">
-            <div className="item">
-              <Input label="EXPIRY DATE" />
-            </div>
-            <div className="item">
-              <Input label="CCV/CVC" />
-            </div>
-          </div>
-          <div className="item">
-            <Input label="CARDHOLDER'S NAME" />
-          </div>
-          <div>
-            <label className="checkbox label">
-              <input type="checkbox" style={{ width: "auto" }} />
-              SAVE CARD
-            </label>
-          </div>
-          <div>
-            <button>ADD CARD</button>
-          </div>
-        </form>
+        <Form />
       </Card>
     </>
   );
